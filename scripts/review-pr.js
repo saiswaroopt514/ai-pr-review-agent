c9ded81f523fa7b79d8ee4b0551b621d2bf0f744
Tummalapalli <venkata.tummalapalli@accenture.com>
2026-06-16 18:46:39 +0530

    Integrate gemini review

--- scripts/review-pr.js
+++ scripts/review-pr.js
@@ -1,32 +1,43 @@
+const { reviewDiff } = require("./gemini");
 const axios = require("axios");
 
 async function main() {
 
-  const owner = process.env.REPO_OWNER;
-  const repo = process.env.REPO_NAME;
-  const prNumber = process.env.PR_NUMBER;
-  const token = process.env.GITHUB_TOKEN;
+    const owner = process.env.REPO_OWNER;
+    const repo = process.env.REPO_NAME;
+    const prNumber = process.env.PR_NUMBER;
+    const token = process.env.GITHUB_TOKEN;
 
-  const response = await axios.get(
-    `https://api.github.com/repos/${owner}/${repo}/pulls/${prNumber}/files`,
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        Accept: "application/vnd.github+json"
-      }
+    const response = await axios.get(
+        `https://api.github.com/repos/${owner}/${repo}/pulls/${prNumber}/files`,
+        {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                Accept: "application/vnd.github+json"
+            }
+        }
+    );
+
+    const files = response.data;
+
+    for (const file of files) {
+
+        console.log("\n=================================");
+        console.log("FILE:", file.filename);
+        console.log("=================================\n");
+
+        if (!file.patch) {
+            continue;
+        }
+
+        const review = await reviewDiff(
+            file.filename,
+            file.patch
+        );
+
+        console.log("\nAI REVIEW\n");
+        console.log(review);
     }
-  );
-
-  const files = response.data;
-
-  for (const file of files) {
-
-    console.log("\n=================================");
-    console.log("FILE:", file.filename);
-    console.log("=================================\n");
-
-    console.log(file.patch || "No patch available");
-  }
 }
 
 main().catch(console.error);
